ae8021c952b4783c3a37af7901e0d46aca06e6ba
Johan Classon <johan@classon.eu>
2018-02-06 10:38:17 +0100

    Refactor

--- poc/ConsoleHost/Program.cs
+++ poc/ConsoleHost/Program.cs
@@ -44,29 +44,4 @@ namespace Wcf.Extensions.OpenIdConnect.Poc.ConsoleHost
             return binding;
         }
     }
-
-    [ServiceContract]
-    public interface IService
-    {
-        [OperationContract]
-        string Ping();
-    }
-
-    // Init option 3: Add behavior by service attribute
-    [WrappedJwtAuthorizationServiceBehavior(requiredScopes: "write")]
-    internal class Service : IService
-    {
-        public string Ping()
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine("Pong");
-
-            foreach (var claim in ClaimsPrincipal.Current.Claims)
-            {
-                sb.AppendFormat("{0} :: {1}\n", claim.Type, claim.Value);
-            }
-
-            return sb.ToString().Trim();
-        }
-    }
 }
---
+++ poc/ConsoleHost/Service.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.ServiceModel;
+using System.Text;
+using Wcf.Extensions.OpenIdConnect.Host;
+
+namespace Wcf.Extensions.OpenIdConnect.Poc.ConsoleHost
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        string Ping();
+    }
+
+    // Init option 3: Add behavior by service attribute
+    [WrappedJwtAuthorizationServiceBehavior(requiredScopes: "write")]
+    internal class Service : IService
+    {
+        public string Ping()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Pong");
+
+            foreach (var claim in ClaimsPrincipal.Current.Claims)
+            {
+                sb.AppendFormat("{0} :: {1}\n", claim.Type, claim.Value);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
