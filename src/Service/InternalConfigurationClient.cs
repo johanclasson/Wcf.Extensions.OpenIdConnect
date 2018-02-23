ea4e6550bfaa104027f8708b7be7499ecf7aff32
Johan Classon <johan@classon.eu>
2018-02-23 15:49:31 +0100

    Refactor

--- src/Service/InternalConfigurationClient.cs
+++ src/Service/InternalConfigurationClient.cs
@@ -14,6 +14,7 @@ namespace Wcf.Extensions.OpenIdConnect.Service
                 dynamic openidConfiguration = JObject.Parse(json);
                 string jwksUri = openidConfiguration.jwks_uri;
                 dynamic jwks = JObject.Parse(await client.GetStringAsync(jwksUri));
+                //TODO: Throw if no key or x5c is present
                 string certificate = jwks.keys[0].x5c[0];
                 string issuer = openidConfiguration.issuer;
                 string accessTokenIssuer = openidConfiguration.access_token_issuer;
